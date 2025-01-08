6855ce635927ba40701bb94c951a76a44d18fd2d
psbvision <punitsinghbisht1529@gmail.com>
2025-01-08 13:38:42 +0530

    intiated header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className}`}>
         <div className="bg-[url('/bg.webp')] opacity-50 fixed -z-10 inset-0"/>
-        {/* <header /> */}
+        <Header />
         <main className="min-h-screen">{children}</main>
         <footer className="bg-orange-300 py-12 bg-opacity-10">
           <div className="mx-auto px-4 text-gray-900 text-center">
